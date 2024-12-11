cafdab54db37be8e8532178780c3c9a675012f63
Nuraulia Octaviana <nurauliaaa12@gmail.com>
2024-12-11 11:31:15 +0700

    Coba

---
+++ app/src/main/java/id/ac/polbeng/aulia/cobaaa/MainActivity.kt
@@ -0,0 +1,10 @@
+package id.ac.polbeng.aulia.githubprofile.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import androidx.lifecycle.ViewModelProvider
+
+class MainActivity : AppCompatActivity() {
+
+}
